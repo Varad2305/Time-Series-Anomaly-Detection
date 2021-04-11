ea5ce7e31c7f4a1336b9b3f66e933c58f35cbf76
Varad2305 <f20170141@hyderabad.bits-pilani.ac.in>
2021-04-11 20:27:50 +0530

    Hyperopt code

--- hyp_opt.py
+++ hyp_opt.py
@@ -1,4 +1,3 @@
-# Imports
 import sys,os
 import numpy as np
 import pandas as pd
@@ -16,26 +15,34 @@ from hyperopt import Trials, STATUS_OK, tpe
 def create_model(X_train,X_test,y_train,y_test):
     #Model begins
     model = Sequential()
+    model.add(Input(shape=(window_size,1)))
     model.add(Bidirectional(LSTM({{choice([4,8,16,32])}},activation='tanh',input_shape=(window_size,1),return_sequences=True)))
     model.add(SeqSelfAttention(attention_activation='sigmoid',name='Attention'))
-    model.add(Flatten())
     model.add(Dropout({{uniform(0,1)}}))
     model.add(Dense(1))
     model.compile(optimizer='adam',loss='mse')
-    return model
+    # return model
+
+
+    # model = Sequential()
+    # model.add(Input(shape=(window_size,1)))
+    # model.add(Bidirectional(LSTM({{choice([4,8,16,32])}},activation='tanh',input_shape=(window_size,1),return_sequences=True)))
+    # model.add(SeqSelfAttention(attention_activation='sigmoid',name='Attention'))
+    # # model.add(Flatten())
+    # model.add(Dropout({{uniform(0,1)}}))
+    # model.add(Dense(1))
+
+    
     #Model ends
 
     #Compiling, fitting
-    model = Model(_input,output)
+    
     model.compile(loss='mse',optimizer='adam',metrics=['mse'])
     history = model.fit(X_train,y_train,batch_size=32,epochs=100,verbose=0,validation_split=0.1)
     validation_loss = np.amin(history.history['val_mse'])
     return {'loss': validation_loss, 'status': STATUS_OK, 'model': model}
 
 def data():
-    """
-    Output : Returns train and test datasets in the required format
-    """
     data_sheet = pd.read_csv(sys.argv[1])
     _,window_size = get_window_size(data_sheet)
     features,values = create_dataset(data_sheet,window_size)
